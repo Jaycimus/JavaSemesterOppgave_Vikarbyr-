57e13d3732b336b4940e6c43aac7b382e39766c5
Jaycimus <dyhre@hotmail.no>
2015-04-22 10:35:57 +0200

    RegistrerVikariat fiks

--- src/javasemesteroppgave_vikarbyrå/RegistrerVikariat.java
+++ src/javasemesteroppgave_vikarbyrå/RegistrerVikariat.java
@@ -1,28 +1,19 @@
-/*
-<<<<<<< HEAD
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javasemesteroppgave_vikarbyrå;
-
-/**
- *
- * @author Arthur
- */
-public class RegistrerVikariat {
-    
-=======
-Gruppemedlemmene:
-Andreas Stenseng Bjørnrud, studentnummer: s236654, INFORMATIK14HA
-Jørgen Dyhre, studentnummer: s236647, INFORMATIK14HA
-Arthur Nordnes, studentnummer: S236644, INFORMATIK14HA
- */
 package javasemesteroppgave_vikarbyrå;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+
+    
+/*
+Gruppemedlemmene:
+Andreas Stenseng Bjørnrud, studentnummer: s236654, INFORMATIK14HA
+Jørgen Dyhre, studentnummer: s236647, INFORMATIK14HA
+Arthur Nordnes, studentnummer: S236644, INFORMATIK14HA
+ */
+
+
+
 
 
 public class RegistrerVikariat extends JPanel {
@@ -31,5 +22,5 @@ public class RegistrerVikariat extends JPanel {
     public RegistrerVikariat (JTextArea utskrift){
         
     }
->>>>>>> origin/master
+
 }
