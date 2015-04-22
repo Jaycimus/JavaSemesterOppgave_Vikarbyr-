cdf7a572174aad068b912f861b9095bd1fd2c122
Jaycimus <dyhre@hotmail.no>
2015-04-22 11:30:19 +0200

    RegistrerVikariet Start

--- src/javasemesteroppgave_vikarbyrå/RegistrerVikariat.java
+++ src/javasemesteroppgave_vikarbyrå/RegistrerVikariat.java
@@ -1,8 +1,20 @@
 package javasemesteroppgave_vikarbyrå;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
     
 /*
@@ -18,8 +30,22 @@ Arthur Nordnes, studentnummer: S236644, INFORMATIK14HA
 
 public class RegistrerVikariat extends JPanel {
     private JButton regVikariat;
+   // JTextField rvt_firm
+  //  private JLabel lbl_
+  //  private JRadioButton
+    
+    private JTextArea utskrift;
+    
+    private JComboBox<String> cb_firmaer;
+    private String[] firmaer = {"Firma 1","Firma 2"};
     
     public RegistrerVikariat (JTextArea utskrift){
+        setLayout(new GridLayout(0,2,20,25));
+        setPreferredSize(new Dimension(500,500));
+        
+        this.utskrift = utskrift;
+        
+        //Knappelytter lytter = new Knappelytter();
         
     }
 
