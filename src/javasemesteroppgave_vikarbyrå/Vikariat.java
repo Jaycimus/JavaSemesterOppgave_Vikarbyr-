cd1866dbeb11e84becac5cf5eb4260612a45ec58
OutCrack <andreas@bjornrud.com>
2015-04-24 00:49:14 +0200

    ads

--- src/javasemesteroppgave_vikarbyrå/Vikariat.java
+++ src/javasemesteroppgave_vikarbyrå/Vikariat.java
@@ -10,6 +10,26 @@ package javasemesteroppgave_vikarbyrå;
 public class Vikariat {
     private String firma;
     private String arbeidssted;
+    //private int vikariatNr;
+    //private Date varighet;
+    private String arbeidstid;
+    private String stillingstype;
+    private String kvalifikasjoner;
+    private String lønnsbetingelser;
+    private String kontaktinfo;
+    private String stillingsInfo;
+    //private SoknadsRegister soknadReg;
+    
+    public Vikariat (String firma, String arbeidssted, String arbeidstid, String stillingstype, String kvalifikasjoner, String lønnsbetingelser, String kontaktinfo, String stillingsInfo){
+        this.firma = firma;
+        this.arbeidssted = arbeidssted;
+        this.arbeidstid = arbeidstid;
+        this.stillingstype = stillingstype;
+        this.kvalifikasjoner = kvalifikasjoner;
+        this.lønnsbetingelser = lønnsbetingelser;
+        this.kontaktinfo = kontaktinfo;
+        this.stillingsInfo = stillingsInfo;
+    }
     
 /*Arbeidssted
 Jobbkategori
