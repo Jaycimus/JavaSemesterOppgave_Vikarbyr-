2569a28a97cb6064a03f684c5cab5cdcdc0d3f03
OutCrack <andreas@bjornrud.com>
2015-04-23 16:57:10 +0200

    Endringer...
    Firma.java
    RegisterFirma.java
    Logginn.java
    Ansattvindu.java

--- src/javasemesteroppgave_vikarbyrå/RegistrerFirma.java
+++ src/javasemesteroppgave_vikarbyrå/RegistrerFirma.java
@@ -10,12 +10,9 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -90,9 +87,7 @@ public class RegistrerFirma extends JPanel {
         add(lbl_epost);
         add(tf_epost);
         add(regFirma);
-        
-        //setVisible(true);
-        
+                        
     }
     
     public JButton getRegFirma(){
