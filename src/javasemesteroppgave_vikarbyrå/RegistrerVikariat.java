1c9037fcf0ceec57f339fc9b3142ea15dec2241b
Jaycimus <dyhre@hotmail.no>
2015-05-05 13:45:13 +0200

    noteringer

--- src/javasemesteroppgave_vikarbyrå/RegistrerVikariat.java
+++ src/javasemesteroppgave_vikarbyrå/RegistrerVikariat.java
@@ -3,7 +3,7 @@ Andreas Stenseng Bjørnrud, studentnummer: s236654, INFORMATIK14HA
 Jørgen Dyhre, studentnummer: s236647, INFORMATIK14HA
 Arthur Nordnes, studentnummer: S236644, INFORMATIK14HA*/
 
-//Sist endret 3. Mai 2015 AV: Andreas Stenseng Bjørnrud
+//Sist endret 5. Mai 2015 AV: Jørgen Dyhre
 package javasemesteroppgave_vikarbyrå;
 
 import java.awt.Dimension;
