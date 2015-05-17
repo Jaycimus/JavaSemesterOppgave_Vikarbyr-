5b670b1df95c56f88a5574b6d2ae2ded1b6ffc2b
Arthur Nordnes <arthurnordnes@gmail.com>
2015-05-17 17:33:26 +0200

    zxkvxzkm
    kcsamclksam

--- src/javasemesteroppgave_vikarbyrå/AnsattVindu.java
+++ src/javasemesteroppgave_vikarbyrå/AnsattVindu.java
@@ -51,7 +51,7 @@ public class AnsattVindu extends JPanel{
         
         borderLayout = new BorderLayout(10, 10);
         setLayout(borderLayout);
-        setBorder(new EmptyBorder(0, 5, 0, 5)); 
+        setBorder(new EmptyBorder(0, 5, 0, 5));
         
         bakgrunn = new Color(0,0,182,155);
                 
